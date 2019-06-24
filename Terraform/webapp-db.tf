a996e176df899cdb88d92655ee0efa126392ece5
RuslanBuryy <50923992+RuslanBuryy@users.noreply.github.com>
2019-06-25 01:32:00 +0300

    Update webapp-db.tf

--- Terraform/webapp-db.tf
+++ Terraform/webapp-db.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rgroup" {
   name     = "${var.resource-group-name}"
   location = "${var.location}"
 }
 
 resource "azurerm_app_service_plan" "plan" {
   name                = "example-appserviceplan"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = "${azurerm_resource_group.rgroup.location}"
+  resource_group_name = "${azurerm_resource_group.rgroup.name}"
 
   sku {
     tier = "Standard"
@@ -16,8 +16,8 @@ resource "azurerm_app_service_plan" "plan" {
 
 resource "azurerm_app_service" "app-service" {
   name                = "${var.app-service-name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = "${azurerm_resource_group.rgroup.location}"
+  resource_group_name = "${azurerm_resource_group.rgroup.name}"
   app_service_plan_id = "${azurerm_app_service_plan.plan.id}"
 
   site_config {
@@ -38,17 +38,17 @@ resource "azurerm_app_service" "app-service" {
 
 resource "azurerm_sql_server" "sqldb" {
   name                         = "terraform-sqlserver"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
-  location                     = "${azurerm_resource_group.rg.location}"
+  resource_group_name          = "${azurerm_resource_group.rgroup.name}"
+  location                     = "${azurerm_resource_group.rgroup.location}"
   version                      = "12.0"
-  administrator_login          = "houssem"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  administrator_login          = "ruslan"
+  administrator_login_password = "Jfeqzvgq521"
 }
 
 resource "azurerm_sql_database" "db" {
   name                = "terraform-sqldatabase"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rgroup.name}"
+  location            = "${azurerm_resource_group.rgroup.location}"
   server_name         = "${azurerm_sql_server.sqldb.name}"
 
   tags = {
