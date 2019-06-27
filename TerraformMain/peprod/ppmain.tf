5daa95e55e4c189427c393b26e3311b67a938c72
RuslanBuryy <50923992+RuslanBuryy@users.noreply.github.com>
2019-06-27 19:13:45 +0300

    Create ppmain.tf

---
+++ TerraformMain/peprod/ppmain.tf
@@ -0,0 +1,65 @@
+terraform {
+  backend          "azurerm"        {}
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "${var.resource-group-name}"
+  location = "${var.location}"
+}
+
+resource "azurerm_app_service_plan" "test" {
+  name                = "pp-appserviceplan"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "test" {
+  name                = "${var.app-service-name}"
+  location            = "${azurerm_resource_group.test.location}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=tcp:${azurerm_sql_server.test.fully_qualified_domain_name} Database=${azurerm_sql_database.test.name};User ID=${azurerm_sql_server.test.administrator_login};Password=${azurerm_sql_server.test.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+  }
+}
+
+resource "azurerm_sql_server" "test" {
+  name                         = "${var.server_name}"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  location                     = "${azurerm_resource_group.test.location}"
+  version                      = "12.0"
+  administrator_login          = "ruslan"
+  administrator_login_password = "Devops2606"
+}
+
+resource "azurerm_sql_database" "test" {
+  name                = "${var.database_name}"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.test.location}"
+  server_name         = "${azurerm_sql_server.test.name}"
+  edition                          = "Basic"
+  collation                        = "SQL_Latin1_General_CP1_CI_AS"
+  create_mode                      = "Default"
+  requested_service_objective_name = "Basic"
+
+  tags = {
+    environment = "ToMaster"
+  }
+}
