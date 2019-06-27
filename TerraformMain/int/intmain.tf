79cc4cd7bca2c3479c756be9d38db9fba99822da
RuslanBuryy <50923992+RuslanBuryy@users.noreply.github.com>
2019-06-27 19:16:02 +0300

    Update intmain.tf

--- TerraformMain/int/intmain.tf
+++ TerraformMain/int/intmain.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_app_service_plan" "test" {
-  name                = "example-appserviceplan"
+  name                = "int-appserviceplan"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
@@ -60,6 +60,6 @@ resource "azurerm_sql_database" "test" {
   requested_service_objective_name = "Basic"
 
   tags = {
-    environment = "ToMaster"
+    environment = "Integration"
   }
 }
