b4c2072821b384da8aae4de148b9f1c2c994638c
RuslanBuryy <50923992+RuslanBuryy@users.noreply.github.com>
2019-06-27 15:26:50 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -37,16 +37,16 @@ resource "azurerm_app_service" "test" {
 }
 
 resource "azurerm_sql_server" "test" {
-  name                         = "terraform-sqlserver"
+  name                         = "course-sqlserver"
   resource_group_name          = "${azurerm_resource_group.test.name}"
   location                     = "${azurerm_resource_group.test.location}"
   version                      = "12.0"
-  administrator_login          = "houssem"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  administrator_login          = "ruslan"
+  administrator_login_password = "devops2606"
 }
 
 resource "azurerm_sql_database" "test" {
-  name                = "terraform-sqldatabase"
+  name                = "course-sqldatabase"
   resource_group_name = "${azurerm_resource_group.test.name}"
   location            = "${azurerm_resource_group.test.location}"
   server_name         = "${azurerm_sql_server.test.name}"
