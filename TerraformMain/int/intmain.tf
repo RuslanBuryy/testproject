79afa4941e86fd1fdae62ac1a44234ef3bcf9819
RuslanBuryy <50923992+RuslanBuryy@users.noreply.github.com>
2019-06-27 20:02:58 +0300

    Update intmain.tf

--- TerraformMain/int/intmain.tf
+++ TerraformMain/int/intmain.tf
@@ -2,15 +2,15 @@ terraform {
   backend          "azurerm"        {}
 }
 
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "int" {
   name     = "${var.resource-group-name}"
   location = "${var.location}"
 }
 
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_app_service_plan" "int" {
   name                = "int-appserviceplan"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${azurerm_resource_group.int.location}"
+  resource_group_name = "${azurerm_resource_group.int.name}"
 
   sku {
     tier = "Standard"
@@ -18,11 +18,11 @@ resource "azurerm_app_service_plan" "test" {
   }
 }
 
-resource "azurerm_app_service" "test" {
+resource "azurerm_app_service" "int" {
   name                = "${var.app-service-name}"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+  location            = "${azurerm_resource_group.int.location}"
+  resource_group_name = "${azurerm_resource_group.int.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.int.id}"
 
   site_config {
     dotnet_framework_version = "v4.0"
@@ -36,24 +36,24 @@ resource "azurerm_app_service" "test" {
   connection_string {
     name  = "Database"
     type  = "SQLServer"
-    value = "Server=tcp:${azurerm_sql_server.test.fully_qualified_domain_name} Database=${azurerm_sql_database.test.name};User ID=${azurerm_sql_server.test.administrator_login};Password=${azurerm_sql_server.test.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+    value = "Server=tcp:${azurerm_sql_server.int.fully_qualified_domain_name} Database=${azurerm_sql_database.int.name};User ID=${azurerm_sql_server.int.administrator_login};Password=${azurerm_sql_server.int.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
   }
 }
 
-resource "azurerm_sql_server" "test" {
+resource "azurerm_sql_server" "int" {
   name                         = "${var.server_name}"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
-  location                     = "${azurerm_resource_group.test.location}"
+  resource_group_name          = "${azurerm_resource_group.int.name}"
+  location                     = "${azurerm_resource_group.int.location}"
   version                      = "12.0"
   administrator_login          = "ruslan"
   administrator_login_password = "Devops2606"
 }
 
-resource "azurerm_sql_database" "test" {
+resource "azurerm_sql_database" "int" {
   name                = "${var.database_name}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  location            = "${azurerm_resource_group.test.location}"
-  server_name         = "${azurerm_sql_server.test.name}"
+  resource_group_name = "${azurerm_resource_group.int.name}"
+  location            = "${azurerm_resource_group.int.location}"
+  server_name         = "${azurerm_sql_server.int.name}"
   edition                          = "Basic"
   collation                        = "SQL_Latin1_General_CP1_CI_AS"
   create_mode                      = "Default"
