6986f0281c2ce07b0158addae4cde9bf82fadc25
RuslanBuryy <50923992+RuslanBuryy@users.noreply.github.com>
2019-06-27 20:27:24 +0300

    Update prodmain.tf

--- TerraformMain/prod/prodmain.tf
+++ TerraformMain/prod/prodmain.tf
@@ -2,15 +2,15 @@ terraform {
   backend          "azurerm"        {}
 }
 
-resource "azurerm_resource_group" "test" {
+resource "azurerm_resource_group" "prod" {
   name     = "${var.resource-group-name}"
   location = "${var.location}"
 }
 
-resource "azurerm_app_service_plan" "test" {
-  name                = "prod-appserviceplan"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+resource "azurerm_aprod_service_plan" "prod" {
+  name                = "prod-aprodserviceplan"
+  location            = "${azurerm_resource_group.prod.location}"
+  resource_group_name = "${azurerm_resource_group.prod.name}"
 
   sku {
     tier = "Standard"
@@ -18,48 +18,48 @@ resource "azurerm_app_service_plan" "test" {
   }
 }
 
-resource "azurerm_app_service" "test" {
-  name                = "${var.app-service-name}"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.test.id}"
+resource "azurerm_aprod_service" "prod" {
+  name                = "${var.aprod-service-name}"
+  location            = "${azurerm_resource_group.prod.location}"
+  resource_group_name = "${azurerm_resource_group.prod.name}"
+  aprod_service_plan_id = "${azurerm_aprod_service_plan.prod.id}"
 
   site_config {
     dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
   }
 
-  app_settings = {
+  aprod_settings = {
     "SOME_KEY" = "some-value"
   }
 
   connection_string {
     name  = "Database"
     type  = "SQLServer"
-    value = "Server=tcp:${azurerm_sql_server.test.fully_qualified_domain_name} Database=${azurerm_sql_database.test.name};User ID=${azurerm_sql_server.test.administrator_login};Password=${azurerm_sql_server.test.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+    value = "Server=tcp:${azurerm_sql_server.prod.fully_qualified_domain_name} Database=${azurerm_sql_database.prod.name};User ID=${azurerm_sql_server.prod.administrator_login};Password=${azurerm_sql_server.prod.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
   }
 }
 
-resource "azurerm_sql_server" "test" {
+resource "azurerm_sql_server" "prod" {
   name                         = "${var.server_name}"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
-  location                     = "${azurerm_resource_group.test.location}"
+  resource_group_name          = "${azurerm_resource_group.prod.name}"
+  location                     = "${azurerm_resource_group.prod.location}"
   version                      = "12.0"
   administrator_login          = "ruslan"
   administrator_login_password = "Devops2606"
 }
 
-resource "azurerm_sql_database" "test" {
+resource "azurerm_sql_database" "prod" {
   name                = "${var.database_name}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  location            = "${azurerm_resource_group.test.location}"
-  server_name         = "${azurerm_sql_server.test.name}"
+  resource_group_name = "${azurerm_resource_group.prod.name}"
+  location            = "${azurerm_resource_group.prod.location}"
+  server_name         = "${azurerm_sql_server.prod.name}"
   edition                          = "Basic"
   collation                        = "SQL_Latin1_General_CP1_CI_AS"
   create_mode                      = "Default"
   requested_service_objective_name = "Basic"
 
   tags = {
-    environment = "ToMaster"
+    environment = "prodaction"
   }
 }
