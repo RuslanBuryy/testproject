6725f99c38ba2e7af25df6037b73c4761c1bda56
RuslanBuryy <50923992+RuslanBuryy@users.noreply.github.com>
2019-06-28 05:26:26 +0300

    Update Employee.sql

--- WebApp.Database/dbo/Tables/Employee.sql
+++ WebApp.Database/dbo/Tables/Employee.sql
@@ -4,7 +4,8 @@
     [Fullname]   NVARCHAR (MAX) NULL,
     [Email]		 NVARCHAR (50)  NULL, 
 	[Phone]		 NVARCHAR (50)  NULL, 
-	[Address]	 NVARCHAR (50)  NULL, 
+	[Address]	 NVARCHAR (50)  NULL,
+	[Address2]	 NVARCHAR (50)  NULL,
     CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
